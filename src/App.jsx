774ce2b841e0b8ef6efb5ffb89b74f9a725dccda
Caylin Vu <caylinvu@gmail.com>
2023-08-21 16:35:26 -0500

    Default to example resume on first load

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,56 @@ import InputControl from './components/InputControl';
 
 function App() {
   const [generalInfo, setGeneralInfo] = useState({
-    name: '',
-    title: '',
-    email: '',
-    phone: '',
-    location: '',
-    summary: '',
+    name: 'Jan Smith',
+    title: 'Software Engineer',
+    email: 'jansmith@fakeemail.com',
+    phone: '123-456-7890',
+    location: 'Portland, Oregon',
+    summary:
+      'Young professional with 5 years of programming experience and a college background in Computer Science. Looking for new career opportunities where I can further improve my skills in web development and work alongside a passionate team in a company whose values align with mine. Open to work in any field where my experience is applicable, and also open to learning new skills.',
   });
   const [genPrev, setGenPrev] = useState(generalInfo);
   const [showGen, setShowGen] = useState(true);
 
-  const [workExp, setWorkExp] = useState([]);
-  const [workPrev, setWorkPrev] = useState([]);
-  const [newWorkId, setNewWorkId] = useState(0);
+  const [workExp, setWorkExp] = useState([
+    {
+      id: 0,
+      company: 'Catmart Inc.',
+      position: 'Software Engineer II',
+      startDate: 'August 2020',
+      endDate: 'present',
+      location: 'Remote',
+      description:
+        'Developed the monitoring framework which interfaces with various applications. Supported new application features, serviced launch, and managed application using automated deployment tools.',
+    },
+    {
+      id: 1,
+      company: 'Meow Electronics',
+      position: 'Software Engineer I',
+      startDate: 'May 2018',
+      endDate: 'July 2020',
+      location: 'Portland, OR',
+      description:
+        'Monitored, supported, and deployed multi-tenant solutions. Developed efficient code to automate repetitive tasks. Improved systems and applications with analytics and ad hoc reporting.',
+    },
+  ]);
+  const [workPrev, setWorkPrev] = useState(workExp);
+  const [newWorkId, setNewWorkId] = useState(2);
   const [showWork, setShowWork] = useState(false);
 
-  const [education, setEducation] = useState([]);
-  const [eduPrev, setEduPrev] = useState([]);
-  const [newEduId, setNewEduId] = useState(0);
+  const [education, setEducation] = useState([
+    {
+      id: 0,
+      degree: "Bachelor's Degree in Computer Science",
+      school: 'University of Portland',
+      startDate: 'August 2013',
+      endDate: 'December 2017',
+      location: 'Portland, OR',
+      description: '',
+    },
+  ]);
+  const [eduPrev, setEduPrev] = useState(education);
+  const [newEduId, setNewEduId] = useState(1);
   const [showEdu, setShowEdu] = useState(false);
 
   return (
@@ -93,15 +125,15 @@ export default App;
 
 // FUTURE TO DOS
 
-// maybe change name font
+// change resume name font
 
-// MAYBE ADD ABILITY TO EXPAND SECTIONS
-
-// MAYBE disable other add button when clicking on add
+// add ability to expand/minimize sections
 
 // set character limits????
 
-// maybe add current/ongoing toggle for dates
+// add current/ongoing toggle for dates
+
+// create new resume page if overflow
 
 //
 
