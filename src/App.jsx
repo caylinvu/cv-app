dd1e2576364a9cdee10557b64d1104271a17397d
Caylin Vu <caylinvu@gmail.com>
2023-08-21 16:46:35 -0500

    Move example data to it's own file

--- src/App.jsx
+++ src/App.jsx
@@ -8,57 +8,19 @@ import DisplayGeneralInfo from './components/generalInfo/DisplayGeneralInfo';
 import DisplayEducation from './components/education/DisplayEducation';
 import DisplayWorkExp from './components/workExp/DisplayWorkExp';
 import InputControl from './components/InputControl';
+import { generalExample, workExample, eduExample } from './data/example-resume';
 
 function App() {
-  const [generalInfo, setGeneralInfo] = useState({
-    name: 'Jan Smith',
-    title: 'Software Engineer',
-    email: 'jansmith@fakeemail.com',
-    phone: '123-456-7890',
-    location: 'Portland, Oregon',
-    summary:
-      'Young professional with 5 years of programming experience and a college background in Computer Science. Looking for new career opportunities where I can further improve my skills in web development and work alongside a passionate team in a company whose values align with mine. Open to work in any field where my experience is applicable, and also open to learning new skills.',
-  });
+  const [generalInfo, setGeneralInfo] = useState(generalExample);
   const [genPrev, setGenPrev] = useState(generalInfo);
   const [showGen, setShowGen] = useState(true);
 
-  const [workExp, setWorkExp] = useState([
-    {
-      id: 0,
-      company: 'Catmart Inc.',
-      position: 'Software Engineer II',
-      startDate: 'August 2020',
-      endDate: 'present',
-      location: 'Remote',
-      description:
-        'Developed the monitoring framework which interfaces with various applications. Supported new application features, serviced launch, and managed application using automated deployment tools.',
-    },
-    {
-      id: 1,
-      company: 'Meow Electronics',
-      position: 'Software Engineer I',
-      startDate: 'May 2018',
-      endDate: 'July 2020',
-      location: 'Portland, OR',
-      description:
-        'Monitored, supported, and deployed multi-tenant solutions. Developed efficient code to automate repetitive tasks. Improved systems and applications with analytics and ad hoc reporting.',
-    },
-  ]);
+  const [workExp, setWorkExp] = useState(workExample);
   const [workPrev, setWorkPrev] = useState(workExp);
   const [newWorkId, setNewWorkId] = useState(2);
   const [showWork, setShowWork] = useState(false);
 
-  const [education, setEducation] = useState([
-    {
-      id: 0,
-      degree: "Bachelor's Degree in Computer Science",
-      school: 'University of Portland',
-      startDate: 'August 2013',
-      endDate: 'December 2017',
-      location: 'Portland, OR',
-      description: '',
-    },
-  ]);
+  const [education, setEducation] = useState(eduExample);
   const [eduPrev, setEduPrev] = useState(education);
   const [newEduId, setNewEduId] = useState(1);
   const [showEdu, setShowEdu] = useState(false);
---
+++ src/data/example-resume.jsx
@@ -0,0 +1,46 @@
+const generalExample = {
+  name: 'Jan Smith',
+  title: 'Software Engineer',
+  email: 'jansmith@fakeemail.com',
+  phone: '123-456-7890',
+  location: 'Portland, Oregon',
+  summary:
+    'Young professional with 5 years of programming experience and a college background in Computer Science. Looking for new career opportunities where I can further improve my skills in web development and work alongside a passionate team in a company whose values align with mine. Open to work in any field where my experience is applicable, and also open to learning new skills.',
+};
+
+const workExample = [
+  {
+    id: 0,
+    company: 'Catmart Inc.',
+    position: 'Software Engineer II',
+    startDate: 'August 2020',
+    endDate: 'present',
+    location: 'Remote',
+    description:
+      'Developed the monitoring framework which interfaces with various applications. Supported new application features, serviced launch, and managed application using automated deployment tools.',
+  },
+  {
+    id: 1,
+    company: 'Meow Electronics',
+    position: 'Software Engineer I',
+    startDate: 'May 2018',
+    endDate: 'July 2020',
+    location: 'Portland, OR',
+    description:
+      'Monitored, supported, and deployed multi-tenant solutions. Developed efficient code to automate repetitive tasks. Improved systems and applications with analytics and ad hoc reporting.',
+  },
+];
+
+const eduExample = [
+  {
+    id: 0,
+    degree: "Bachelor's Degree in Computer Science",
+    school: 'University of Portland',
+    startDate: 'August 2013',
+    endDate: 'December 2017',
+    location: 'Portland, OR',
+    description: '',
+  },
+];
+
+export { generalExample, workExample, eduExample };
